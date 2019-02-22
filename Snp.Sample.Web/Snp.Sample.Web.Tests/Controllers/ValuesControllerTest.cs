6a76eb978f2d67b263546afbe79b9b0e4589e7f3
Christian Tiquia <christian.tiquia@spglobal.com>
2019-02-22 14:32:42 -0500

    Initial Sample web

---
+++ Snp.Sample.Web/Snp.Sample.Web.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using Snp.Sample.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Snp.Sample.Web.Tests.Controllers
+{
+    public class ValuesControllerTest
+    {
+        public ValuesControllerTest()
+        {
+
+        }
+
+        [Fact]
+        public void Get_ReturnsValues_WithAListOfString()
+        {
+            // Arrange
+            var controller = new ValuesController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.Equal(2, result.Value.Count());
+        }
+
+
+        [Fact]
+        public void Get_ReturnsValues_WithAStringValue()
+        {
+            // Arrange
+            var controller = new ValuesController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.Equal("value1", result.Value.FirstOrDefault());
+        }
+
+
+        [Fact]
+        public void GetById_ReturnsSingleValue_WithAStringValue()
+        {
+            // Arrange
+            var controller = new ValuesController();
+
+            // Act
+            var result = controller.Get(1);
+
+            // Assert
+            Assert.Equal("value", result.Value);
+        }
+    }
+}
